325b0bc48477cdb661c6660070fc6a2059bfd2fa
Douglas McGowan <dmcgowan@iinet.net.au>
2020-04-04 15:13:14 +1100

    Removing the mfa condition as this wont plan nice with codebuild

--- modules/aws/modules/roles/main.tf
+++ modules/aws/modules/roles/main.tf
@@ -6,17 +6,9 @@ module "iam_assumable_role" {
     source  = "terraform-aws-modules/iam/aws//modules/iam-assumable-role"
     version = "~> 2.0"
 
+    role_requires_mfa       = false
     role_name               = "${var.project_code}-${var.name}"
     create_role             = true
     custom_role_policy_arns = var.policy_arns
     trusted_role_services   = var.trusted_services
-
-    # trusted_role_services = [
-    #     "codebuild.amazonaws.com"
-    # ]
-
-    # custom_role_policy_arns = [
-    #     "arn:aws:iam::aws:policy/AmazonCognitoReadOnly",
-    #     "arn:aws:iam::aws:policy/AlexaForBusinessFullAccess",
-    # ]
 }
