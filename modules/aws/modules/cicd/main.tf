932c129264ec4b1f05967f546a772b99ea5c14dc
Douglas McGowan <dmcgowan@iinet.net.au>
2020-05-23 23:22:12 +1000

    buildspec : resetting image type

--- modules/aws/modules/cicd/main.tf
+++ modules/aws/modules/cicd/main.tf
@@ -48,7 +48,7 @@ resource "aws_codebuild_project" "codebuild" {
     #Keep environment as generic as possible
     environment {
         compute_type    = "BUILD_GENERAL1_SMALL"
-        image           = "aws/codebuild/docker:18"
+        image           = "aws/codebuild/standard:2.0"
         type            = "LINUX_CONTAINER"
         privileged_mode = true
     }
