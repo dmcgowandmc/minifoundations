ce9a7f5d185e192ed17a8dfc5131042bb91747fe
Douglas McGowan <dmcgowan@iinet.net.au>
2020-05-23 23:18:38 +1000

    buildspec : increasing priviledge

--- modules/aws/modules/cicd/main.tf
+++ modules/aws/modules/cicd/main.tf
@@ -47,9 +47,10 @@ resource "aws_codebuild_project" "codebuild" {
 
     #Keep environment as generic as possible
     environment {
-        compute_type = "BUILD_GENERAL1_SMALL"
-        image        = "aws/codebuild/standard:2.0"
-        type         = "LINUX_CONTAINER"
+        compute_type    = "BUILD_GENERAL1_SMALL"
+        image           = "aws/codebuild/docker:18"
+        type            = "LINUX_CONTAINER"
+        privileged_mode = true
     }
 }
 
