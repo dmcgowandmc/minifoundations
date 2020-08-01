cef3a8d879b8321aba2c21a930d1be37631bc8fe
Douglas McGowan <dmcgowan@iinet.net.au>
2020-08-01 11:31:56 +1000

    FIxing route 53 to support private zones properly. Added in tags as well

--- modules/aws/modules/route53/variables.tf
+++ modules/aws/modules/route53/variables.tf
@@ -2,6 +2,20 @@
 ## All variables for creation of route 53 zones resides here ##
 ###############################################################
 
+#Environment code used with customer code and project code to create a unique identifier when used outside foundations. For route53, only part of the tag strategy
+variable "environment_code" {
+    type        = string
+    description = "2 - 4 letter prefix identifying your environment. Used with customer code and project code for unique identifier when used outside foundations"
+    default     = ""
+}
+
+#Customer code used with environment code and project code to create a unique identifier when used outside foundations. For route53, only part of the tag strategy
+variable "customer_code" {
+    type        = string
+    description = "Small prefix indentifying the customer. Used with environment code and project code for unique identifier when used outside foundations"
+    default     = ""
+}
+
 #Project code forms part of the naming convention
 variable "project_code" {
     type        = string
@@ -19,6 +33,12 @@ variable "force_destory" {
     type        = bool
     description = "Set to true to force this zone to be destroyed along with records where a destroy and recreate is required"
     default     = true
+}
+
+variable "tags" {
+    description = "Optional map of additional tags for the route53 zone"
+    type        = map(string)
+    default     = {}
 }
 
 variable "vpc_id" {
