c44b13514d95f1ed13e75124957acc4bdc44542f
Douglas McGowan <dmcgowan@iinet.net.au>
2020-08-07 21:01:57 +1000

    Removing STS policy. This should reside in CICD

--- modules/aws/modules/roles/main.tf
+++ modules/aws/modules/roles/main.tf
@@ -32,39 +32,3 @@ module "iam_assumable_role" {
     trusted_role_services   = var.trusted_services
     tags                    = local.tags
 }
-
-#Create and attach a custom policy that allows use of the secure token service (sts)
-
-#Why? Within docker, we have to actually pass in credentials, fixed or otherwise. So beforehand, we need to explicitly call sts, obtain the 
-#temporary credentials, and pass to docker as input variables
-
-#Create the custom policy
-resource "aws_iam_policy" "assume-policy" {
-    #FIX REQUIRED
-    name        = "${local.role_name}-assume-sts"
-    path        = "/"
-    description = "Enable use of the secure token service for ${local.role_name}"
-
-    policy = <<EOF
-{
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Sid": "stsAssumeRole",
-            "Effect": "Allow",
-            "Action": [
-                "sts:AssumeRole",
-                "sts:GetCallerIdentity"
-            ],
-            "Resource": "${module.iam_assumable_role.this_iam_role_arn}"
-        }
-    ]
-}
-EOF
-}
-
-#Attach the custom policy to the previosly created role
-resource "aws_iam_role_policy_attachment" "assume-policy-to-assumeable-role" {
-    role       = module.iam_assumable_role.this_iam_role_name
-    policy_arn = aws_iam_policy.assume-policy.arn
-}
