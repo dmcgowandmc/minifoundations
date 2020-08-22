288dc998145cd958fb87e075161d23bce440a43f
Douglas McGowan <dmcgowan@iinet.net.au>
2020-08-22 17:02:28 +1000

    Fixing names from copy paste

--- modules/aws/modules/sns/main.tf
+++ modules/aws/modules/sns/main.tf
@@ -4,15 +4,15 @@
 
 locals {
     #Resource code
-    s3_resource_code = "sns"
+    sns_resource_code = "sns"
 
     #Tag Settings
-    sns_name = var.customer_code != "" && var.environment_code != "" ? "${var.project_code}-${local.s3_resource_code}-${var.customer_code}-${var.environment_code}-${var.sns_name}" : "${var.project_code}-${local.s3_resource_code}-${var.sns_name}"
+    sns_name = var.customer_code != "" && var.environment_code != "" ? "${var.project_code}-${local.sns_resource_code}-${var.customer_code}-${var.environment_code}-${var.sns_name}" : "${var.project_code}-${local.sns_resource_code}-${var.sns_name}"
     tags = merge(
         {
             "Name"             = local.sns_name,
             "Project Code"     = var.project_code,
-            "Resource Code"    = local.s3_resource_code,
+            "Resource Code"    = local.sns_resource_code,
             "Customer Code"    = var.customer_code == "" ? "NA" : var.customer_code,
             "Environment Code" = var.environment_code == "" ? "NA" : var.environment_code
         },
